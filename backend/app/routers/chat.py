54907f146103b8f7635e80d5d2d3ff01b32fd7bf
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-05-28 03:01:48 -0300

    feat: Modificar para cargar y guardar memoria de conversación

--- backend/app/routers/chat.py
+++ backend/app/routers/chat.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 from app.models.message import Message
 from app.services.ollama_service import send_ollama
 
+from app.services.memory_service import load_memory, save_memory
+
 router = APIRouter()
 
 @router.post("/chat")
@@ -10,7 +12,11 @@ async def send_chat(message: Message):
     
     response = await send_ollama(user_msg)
     
-    return {
+    response_obj = {
         "user_message": user_msg,
         "sophia_response": response
     }
+    
+    save_memory(load_memory(),response_obj)
+    
+    return response_obj 
--- backend/app/services/ollama_service.py
+++ backend/app/services/ollama_service.py
@@ -1,13 +1,16 @@
 import httpx
 from app.ai.sophia_identity import load_sophia_identity
+from app.services.memory_service import load_memory
 
 async def send_ollama(text: str, system: str = ""):
     
     system = load_sophia_identity()
     
+    historial = str(load_memory()[-5::])
+    
     prompt = {
             "model": "qwen3.5:9b",
-            "prompt": text,
+            "prompt": historial + '{"user_message":'+text+'}',
             "system": system,
             "stream": False,
             "think": False
@@ -16,4 +19,5 @@ async def send_ollama(text: str, system: str = ""):
     async with httpx.AsyncClient(timeout=180.0) as client:
         response = await client.post("http://localhost:11434/api/generate", json=prompt)
         data = response.json()
+    
     return data["response"]
