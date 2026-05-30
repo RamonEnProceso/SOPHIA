bab5d3d4b3b8fa1891e80a09754d4f45dd0eba0a
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-05-30 02:51:27 -0300

    feat: Guardar chat en el historial de la base de datos

--- backend/app/services/memory_service.py
+++ backend/app/services/memory_service.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from app.models.chat import chat
+from app.repositories.messages_repository import insert_message
 import json
 
 path = Path(__file__).resolve().parent.parent.parent / ".." / "data" / "history.json"
@@ -13,15 +15,13 @@ def load_memory():
     
     return content
 
-def save_memory(memory, new_chat):
+def save_memory(memory, new_chat: chat):
     
     memory.append(new_chat)
+    
+    insert_message(1, new_chat["user_message"], new_chat["sophia_response"])
     
     with open(path, 'w', encoding="utf-8") as file:
         json.dump(memory, file, ensure_ascii=False, indent=4)
     
     return
-
-
-def append_new(new):
-    return load_memory().append(new)
