dc5580b078fee8b6c41357ffdea1eb1605b82ec9
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-05-30 02:53:34 -0300

    feat: Añadir escritura y lectura de datos a la tabla de messages de la base de datos

---
+++ backend/app/repositories/messages_repository.py
@@ -0,0 +1,56 @@
+from app.services.psql_service import get_connection
+import psycopg2
+
+def get_messages():
+    
+    conn = get_connection()
+    
+    try:
+        with conn.cursor() as curs:
+            curs.execute("SELECT * FROM messages")
+            rows = curs.fetchall()
+        return rows
+    
+    except (Exception, psycopg2.Error) as err:
+        print(err)
+        
+    finally:
+        conn.close()
+
+def insert_message(
+    user_id,
+    user_message,
+    sophia_response 
+):
+    conn = get_connection()
+    
+    try:
+        with conn.cursor() as curs:
+            curs.execute(
+                "INSERT INTO messages (user_id, user_message, sophia_response) VALUES (%s, %s, %s)",
+                (user_id,user_message,sophia_response)
+            )
+        conn.commit()
+    
+    except (Exception, psycopg2.Error) as err:
+        print(err)
+        
+    finally:
+        conn.close()
+        
+def delete_message(id):
+    conn = get_connection()
+    
+    try:
+        with conn.cursor() as curs:
+            curs.execute("DELETE FROM messages WHERE id = %s",(id,))
+        conn.commit()
+    
+    except (Exception, psycopg2.Error) as err:
+        print(err)
+        
+    finally:
+        conn.close()
+
+if __name__ == "__main__":
+    print(get_messages())
